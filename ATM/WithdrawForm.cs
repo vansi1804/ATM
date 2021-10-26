3882e6b846a65b8890af2e12cc2e42a178e57ec6
KnightSiz <vansi18042001@gmail.com>
2021-10-26 22:02:37 +0700

    first commit

---
+++ ATM/WithdrawForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ATM
+{
+    public partial class WithdrawForm : Form
+    {
+        ATMEntities dbContext = new ATMEntities();
+        Account account = null;
+
+        public WithdrawForm(Account account)
+        {
+            InitializeComponent();
+            this.account = account;
+            Load();
+        }
+        
+        private void Load()
+        {
+            txtAmount.Text = "";
+            txtBalance.Text = account.Balance.ToString();
+            txtBalance.Enabled = false;
+        }
+
+        private void btnWithdraw_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
